d0e5c4e975f0b7a80b67cbdbfe2d0aad70fcc0d5
Gabrielsuigo <gaby.jjr10@gmail.com>
2025-08-02 20:17:36 -0300

    Actualizacion Front

--- components/Navbar/SearchBar.tsx
+++ components/Navbar/SearchBar.tsx
@@ -3,11 +3,11 @@
 import { SearchBarProps } from "@/app/interfaces";
 
 const SearchBar = ({ value, onChange }: SearchBarProps) => (
-  <div className="flex items-center bg-white dark:bg-neutral-800 rounded-xl px-3 py-1 w-full shadow-md transition-colors duration-300">
+  <div className="flex items-center bg-neutral-100 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 rounded-xl px-3 py-1 w-full shadow-sm hover:shadow-md transition-all duration-300">
     <input
       type="text"
       placeholder="Buscar productos..."
-      className="bg-transparent text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 outline-none w-full py-2 px-2 rounded-lg focus:ring-2 focus:ring-black dark:focus:ring-white transition-all duration-300"
+      className="bg-transparent text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 outline-none w-full py-2 px-2 rounded-lg focus:ring-2 focus:ring-neutral-600 dark:focus:ring-neutral-400 transition-all duration-300"
       value={value}
       onChange={onChange}
     />
